61e1396d49246cf23149174bb60e570fb1d084c6
shizop <44642109+shizop@users.noreply.github.com>
2024-04-19 13:14:10 +0800

    update github url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather</div>;
+  return (
+    <div className="App">
+      <h1>Weather</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://voluble-entremet-baa96d.netlify.app/">Syaibatul </a>
+        and is{" "}
+        <a href="https://github.com/shizop/my-weather-react">
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
